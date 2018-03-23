2b48998871e37c267b47bfe866fb904dd049a474
Ray D. Sameshima <rsameshima@citytech.cuny.edu>
2018-03-22 21:54:38 -0400

    ABC087B: readLn

---
+++ ABC087B.hs
@@ -0,0 +1,19 @@
+{-
+ABC087B.hs
+
+
+-}
+
+main :: IO ()
+main = do
+  a <- readLn
+  b <- readLn 
+  c <- readLn 
+  x <- readLn 
+  print $ selection a b c x
+
+selection
+  :: Int -> Int -> Int -> Int -> Int
+selection a b c x
+  = length [(s,t,u) | s<-[0..a], t<-[0..b], u<-[0..c]
+                    , 500*s + 100*t + 50*u == x]
