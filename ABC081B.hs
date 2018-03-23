146a696775a26fcaac299d3e6f2580c1bd6bc5e4
Ray D. Sameshima <rsameshima@citytech.cuny.edu>
2018-03-22 21:27:51 -0400

    ABC087B: accumulator

---
+++ ABC081B.hs
@@ -0,0 +1,28 @@
+{-
+ABC081B.hs
+
+*Main> let as = [8,12,40]
+*Main> and $ map even as
+True
+*Main> and $ map even $ map (`div` 2) as
+True
+*Main> and $ map even $ map (`div` 2) $ map (`div` 2) as
+False
+-}
+
+main :: IO ()
+main = do
+  n <- getLine
+  as <- map read . words <$> getLine
+  print $ counter as
+
+counter
+  :: [Int] -> Int
+counter ns = helper ns 0
+  where
+    helper :: [Int] -> Int -> Int
+    helper ms k
+      | and $ map even ms = helper ms' (k+1)
+      | otherwise         = k
+      where
+        ms' = map (`div` 2) ms
