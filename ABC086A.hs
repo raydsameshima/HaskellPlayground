eaff49b75856299967c36034afe52fe66ee3ad81
Ray D. Sameshima <rsameshima@citytech.cuny.edu>
2018-03-22 21:27:20 -0400

    ABC086A; again

--- ABC086A.hs
+++ ABC086A.hs
@@ -6,21 +6,12 @@ https://beta.atcoder.jp/contests/abs/tasks/abc086_a
 main :: IO ()
 main = do
   [a,b] <- toListOfInt
---  let c = oddOrEven (a*b)
---  putStrLn c
   putStrLn $ oddOrEven' a b
 
 -- To read spaced inputs.
 toListOfInt 
   :: Read b => IO [b]
 toListOfInt = map read . words <$> getLine
-
-oddOrEven
-  :: Int -> String
-oddOrEven n
-  | even n    = "Even"  
-  | odd  n    = "Odd"
-  | otherwise = error "oddOrEven: "
 
 oddOrEven'
   :: Int -> Int -> String
