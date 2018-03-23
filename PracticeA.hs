b16a2389c4e72746dff2d21c062a9df1c041ec6a
Ray D. Sameshima <rsameshima@citytech.cuny.edu>
2018-03-22 21:03:31 -0400

    PracticeA, done

---
+++ PracticeA.hs
@@ -0,0 +1,16 @@
+{-
+PracticeA.lhs
+
+https://beta.atcoder.jp/contests/abs/tasks/practice_1
+-}
+
+main :: IO ()
+main = do
+  a <- readLn
+  [b, c] <- toListOfInt
+  s <- getLine
+  putStrLn $ show (a+b+c) ++ " " ++ s
+
+toListOfInt 
+  :: Read b => IO [b]
+toListOfInt = map read . words <$> getLine
