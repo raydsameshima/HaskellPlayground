0c041266683e70ace334c4ae09ca19fb12c4ff60
raydsameshima <ray.d.sameshima@gmail.com>
2019-01-31 20:46:08 -0500

    ABC082A, type annotation does not provide speed

--- ABC082A.hs
+++ ABC082A.hs
@@ -1,13 +1,20 @@
-{- ABC082.hs -}
+{- ABC082.hs
+
+Given two integers
+  a b
+print the rounded number of the average
+  (a+b)/2
+.
+
+-}
 
 main :: IO ()
 main = do
   [a,b] <- map read . words <$> getLine
-  print $ kiriage (a+b)
+  print $ (a+b+1) `div` 2
 
 kiriage 
   :: Int -> Int 
 kiriage n 
   | odd  n = (n+1) `div` 2
   | even n = n     `div` 2
-
