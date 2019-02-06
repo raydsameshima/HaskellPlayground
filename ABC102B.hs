04d8d2456fd7d695bfeb710791499bdac904f67c
raydsameshima <ray.d.sameshima@gmail.com>
2019-02-05 21:38:42 -0500

    ABC102B; 1msec 508KB done.

--- ABC102B.hs
+++ ABC102B.hs
@@ -1,27 +1,31 @@
 {- ABC102B.hs
 
 Given
-  N
+  N (>= 2)
   A1 .. AN
 of integers, find the maximum absolute difference of two elements.
 
+Since both B.head and B.last are O(1), we can simply sort as and take head and last.
+
 -}
 
-main :: IO ()
+import qualified Data.ByteString.Char8 as C
+import Data.Maybe 
+  ( fromJust )
+
 main = do
   _  <- getLine
-  as <- map read . words <$> getLine :: IO [Int]
-  print . f $ as
+  as <- map (fst . fromJust . C.readInt) . C.words <$> C.getLine 
+  print $ f as
 
 f :: [Int] -> Int
 f (a:b:bs) 
   | a < b     = g (a,b) bs
   | otherwise = g (b,a) bs
-
+ 
 g :: (Int,Int) -> [Int] -> Int
 g (s,l) [] = l-s
 g sl@(s,l) (x:xs)
   | x < s = g (x,l) xs
   | l < x = g (s,x) xs
   | otherwise = g sl xs
-
