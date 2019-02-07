88c8f941098aef54d0ef1d042bd0b4a2234d5a29
raydsameshima <ray.d.sameshima@gmail.com>
2019-02-07 13:58:14 -0500

    ABC072B; the rare implementation is faster than zip-usage.

--- AtCoder/ABC072B.hs
+++ AtCoder/ABC072B.hs
@@ -1,6 +1,11 @@
 {- 
 ABC072B.hs
 
+Given
+  s
+of string, print the odd-indexd positions of s, where the index is
+  [1 .. ]
+
 *Main> as = "atcoder"
 *Main> zip [1..] as
 [(1,'a'),(2,'t'),(3,'c'),(4,'o'),(5,'d'),(6,'e'),(7,'r')]
@@ -11,13 +16,16 @@ ABC072B.hs
 -}
 
 main :: IO ()
-main = interact pickOdds
+main = interact pickOdds -- non-prime performs well
 
 pickOdds, pickOdds'
   :: [a] -> [a]
 pickOdds []       = []
 pickOdds [a]      = [a]
-pickOdds (a:b:bs) = a : pickOdds bs 
+pickOdds (a:_:bs) = a : pickOdds bs 
 
 pickOdds' = map snd . filter (odd . fst) . zip [1..]
 
+pickOddsL
+  :: [a] -> [a]
+pickOddsL ls = [x | (x,n) <- zip ls [1..], odd n]
