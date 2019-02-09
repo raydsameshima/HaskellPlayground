465df019888e8b687a508242851100e60a486a2f
raydsameshima <ray.d.sameshima@gmail.com>
2019-02-08 22:52:27 -0500

    ABC080B; both perfom well

--- AtCoder/ABC080B.hs
+++ AtCoder/ABC080B.hs
@@ -1,4 +1,6 @@
-{- ABC080B.hs -}
+{- ABC080B.hs 
+
+-}
 
 import Data.Char
   ( digitToInt )
@@ -14,7 +16,13 @@ isHarshad x
   = let fx = sumDigits x  
     in 0 == x `rem` fx
 
-sumDigits
+sumDigits, sumDigits'
   :: Int -> Int
 sumDigits = sum . map digitToInt . show
 
+sumDigits' = sum . int2lst
+int2lst n
+  | q == 0 = [r]
+  | otherwise = r : int2lst q
+  where
+    (q,r) = divMod n 10
