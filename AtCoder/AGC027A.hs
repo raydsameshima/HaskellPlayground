e4bd5dddb73a20b3f721a76e7c7234d240cc3bf1
raydsameshima <ray.d.sameshima@gmail.com>
2019-02-11 00:29:45 -0500

    AGC027A; using Vector does not perform well, yes I know. Let me next try sequence

---
+++ AtCoder/AGC027A.hs
@@ -0,0 +1,32 @@
+{- AGC027A.hs 
+
+Given
+  n x
+  a1 .. an
+of integers, let us spread x candies in n kids, where i-th kid is happy
+when receives ai candies.
+Find the maximum possible number of happy kids.
+
+-}
+
+import Control.Monad (replicateM)
+import Data.List (minimum, scanl', sort, sortBy)
+import qualified Data.Vector.Unboxed as U
+
+main :: IO ()
+main = do
+  [n,x] <- map read . words <$> getLine 
+  as    <- map read . words <$> getLine
+  print $ f n x as
+
+f :: Int -> Int -> [Int] -> Int
+f n x as
+  | x < minas = 0
+  | x <= sas  = U.length $ U.takeWhile (<= x) $ U.scanl1 (+) as'
+  | x < 2*sas = U.length $ U.takeWhile (<= (2*sas - x)) $ U.scanl1 (+) sa
+  | otherwise = 0
+  where
+    minas = U.minimum $ U.fromList as
+    sas = sum as
+    as' = U.fromList $ sort as
+    sa  = U.reverse as' 
