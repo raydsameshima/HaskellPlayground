d9949d6747604ebb1602ce774d4045e97be17048
raydsameshima <ray.d.sameshima@gmail.com>
2019-02-17 18:03:17 -0500

    ABC085C, an iterative search is not a clever idea.

--- AtCoder/ABC085C.hs
+++ AtCoder/ABC085C.hs
@@ -1,29 +1,46 @@
 {- ABC085C.hs
 -}
 
+import qualified Data.ByteString.Char8 as C
+import           Data.Maybe ( fromJust )
+import qualified Data.Vector.Unboxed as U
+
+cReadLn :: IO Int
+cReadLn = fst . fromJust . C.readInt <$> C.getLine
+
+getParms :: IO [Int]
+getParms = map (fst . fromJust . C.readInt) . C.words <$> C.getLine
+
 main :: IO ()
 main = do
-  [n,y] <- map read . words <$> getLine
-  otsdm n y
+  [n,y] <- getParms
+  putStrLn $ otsdm n y
+
+type Yen = Int
+type Mai = Int
+type Pochi = (Int,Int,Int) -- #s of 10000,5000,1000 bills
 
 otsdm'
-  :: Int -> Int -> [(Int,Int,Int)]
+  :: Mai -> Yen -> [Pochi]
 otsdm' n y
-  = [(a,b,c) | let a0 = y `quot` 10000
-             , a <- [0 .. a0]
-             , let b0 = (y - 10000*a) `quot` 5000
-             , b <- [0 .. b0]
-             , let c = n-a-b
-             , a*10000 + b*5000 + c*1000 == y
-             ]
+  = [(a,b,n-a-b) 
+      | let a0 = y `quot` 10000
+      , a <- [0 .. a0]
+      , let b0 = (y - 10000*a) `quot` 5000
+      , b <- [0 .. b0]
+      ]
+
+gok
+  :: Pochi -> Yen
+gok (a,b,c) = 10000*a +5000*b + 1000*c 
 
 otsdm
-  :: Int -> Int -> IO ()
+  :: Mai -> Yen -> String
 otsdm n y
-  | null o    = putStrLn "-1 -1 -1"
-  | otherwise = putStrLn $ show a ++ " " ++ show b ++ " " ++ show c
+  | null os   = "-1 -1 -1"
+  | otherwise = show a ++ " " ++ show b ++ " " ++ show c
   where
-    o = otsdm' n y
-    (a,b,c) = head o
+    os = filter (\o -> y == gok o) $ otsdm' n y
+    (a,b,c) = head os
 
-
+  
