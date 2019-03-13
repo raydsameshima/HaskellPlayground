79f5aa7bebdc6664a5f4fd4335482573a0e67fa5
raydsameshima <ray.d.sameshima@gmail.com>
2019-03-12 23:56:21 -0400

    AGC011A, even if I use unboxed vector, the execution time is on the border.

--- AtCoder/AGC011A.hs
+++ AtCoder/AGC011A.hs
@@ -1,9 +1,10 @@
 {- AGC011A.hs -}
 
 import qualified Data.ByteString.Char8 as C
-import Data.Maybe 
-import Control.Monad
-import Data.List
+import Data.Maybe (isJust, fromJust)
+import Control.Monad (replicateM)
+import Data.List (sort, drop, length, filter, take)
+import qualified Data.Vector.Unboxed as U
 
 cReadLn :: IO Int
 cReadLn = fst . fromJust . C.readInt <$> C.getLine
@@ -14,31 +15,24 @@ getParms = map (fst . fromJust . C.readInt) . C.words <$> C.getLine
 main :: IO ()
 main = do
   [n,c,k] <- getParms -- passengers, maximum num in a bus, and limit time
-  ts <- sort <$> replicateM n cReadLn -- i-th passenger's arrival time
+  ts <- (U.fromList . sort) <$> replicateM n cReadLn -- i-th passenger's arrival time
   print $ f c k ts
 
-f :: Int -> Int -> [Int] -> Int
-f c k ts = solver c k ([],ds) 0
+f :: Int -> Int -> U.Vector Int -> Int
+f c k ts = solver c k ts 0
   where
-    ds = g k ts
-    solver _ _ (_,[]) n = n
-    solver c k (_,ts) n = solver c k (ps,qs) (n+1)
-      where ps = passengersInABus c ts
-            qs = drop (length ps) ts
+    solver :: Int -> Int -> U.Vector Int -> Int -> Int
+    solver _ _ ts n 
+      | U.null ts = n
+    solver c k ts n = solver c k qs (n+1)
+      where p  = inABus c k ts
+            qs = U.drop p ts
 
-type Dt = (Int, Int)
+overWrap :: Int -> Int -> Int -> Int
+overWrap k t0 t1 = (t0+k - t1)
 
-g :: Int -> [Int] -> [Dt]
-g k ts = zip ts (map (+k) ts)
-
-overWrap 
-  :: Dt -> Dt -> Maybe Dt
-overWrap (ai,af) (bi,bf)
-  | af <  bi = Nothing
-  | bi <= af = Just (bi,af)
-
-passengersInABus 
-  :: Int -> [Dt] -> [Maybe Dt]
-passengersInABus c tt@(t:ts) = filter isJust . map (overWrap t) . take c $ tt
-
-
+inABus 
+  :: Int -> Int -> U.Vector Int -> Int
+inABus c k tt = U.length . U.filter (0<=) . U.map (overWrap k t) . U.take c $ tt
+  where
+    t = U.head tt
