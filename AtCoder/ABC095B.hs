bd4d43191d78d3cb48a8a25857a8832f3051e5c6
raydsameshima <ray.d.sameshima@gmail.com>
2019-05-02 00:25:18 -0400

    ABC095B, let me retry this.

--- AtCoder/ABC095B.hs
+++ AtCoder/ABC095B.hs
@@ -5,18 +5,33 @@ Given
   m1
   ...
   mN
-print the maximum number of doughnuts we can make under the following condition:
+of ints, print the maximum number of doughnuts 
+we can make under the following condition:
+
   For each of the N kinds, make at least one.
 
 -}
 
+import qualified Data.ByteString.Char8 as C
+import Data.Maybe ( fromJust )
 import Control.Monad (replicateM)
+
+readInt :: IO Int
+readInt = fst . fromJust . C.readInt <$> C.getLine
+
+readInts :: IO [Int]
+readInts = map (fst . fromJust . C.readInt) . C.words <$> C.getLine
 
 main :: IO ()
 main = do
-  [n,x] <- map read . words <$> getLine 
-  ms    <- replicateM n readLn 
+  [n,x] <- readInts
+  ms    <- replicateM n readInt
   print $ f n x ms
 
+sumMin :: [Int] -> (Int, Int)
+sumMin = (,) <$> sum <*> minimum
+
 f :: Int -> Int -> [Int] -> Int
-f n x ms = n + (x - sum ms) `div` (minimum ms)
+f n x ms = n + (x - sms) `div` minms
+  where
+    (sms, minms) = sumMin ms
