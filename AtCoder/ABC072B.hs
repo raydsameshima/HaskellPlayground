47981c4e054e0e4cedfa5bbb075b7f406b9e0896
raydsameshima <ray.d.sameshima@gmail.com>
2019-05-02 00:09:57 -0400

    ABC072B, interact is somehow tricky, but ok. Do not write homemade recursion, use buildin.

--- AtCoder/ABC072B.hs
+++ AtCoder/ABC072B.hs
@@ -6,26 +6,16 @@ Given
 of string, print the odd-indexd positions of s, where the index is
   [1 .. ]
 
-*Main> as = "atcoder"
-*Main> zip [1..] as
-[(1,'a'),(2,'t'),(3,'c'),(4,'o'),(5,'d'),(6,'e'),(7,'r')]
-*Main> filter (odd . fst) it
-[(1,'a'),(3,'c'),(5,'d'),(7,'r')]
-*Main> map snd it
+*Main> zip "atcoder" $ cycle [True, False]
+[('a',True),('t',False),('c',True),('o',False),('d',True),('e',False),('r',True)]
+*Main> map fst $ filter snd $ it
 "acdr"
+
 -}
 
 main :: IO ()
 main = interact pickOdds -- non-prime performs well
 
-pickOdds, pickOdds'
+pickOdds
   :: [a] -> [a]
-pickOdds []       = []
-pickOdds [a]      = [a]
-pickOdds (a:_:bs) = a : pickOdds bs 
-
-pickOdds' = map snd . filter (odd . fst) . zip [1..]
-
-pickOddsL
-  :: [a] -> [a]
-pickOddsL ls = [x | (x,n) <- zip ls [1..], odd n]
+pickOdds = map snd . filter fst . zip (cycle [True, False])
