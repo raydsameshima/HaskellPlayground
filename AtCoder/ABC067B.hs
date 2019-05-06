a1d08eb692fa06f15cd02eb5a33632a16d61d32e
raydsameshima <ray.d.sameshima@gmail.com>
2019-05-05 22:23:26 -0400

    ABC067B, a mysterious flip.

--- AtCoder/ABC067B.hs
+++ AtCoder/ABC067B.hs
@@ -2,15 +2,24 @@
 
 -}
 
-import Data.List
-  ( sortBy )
+import Data.List ( sort, sortBy )
+import qualified Data.ByteString.Char8 as C
+import Data.Maybe ( fromJust )
+
+readInts :: IO [Int]
+readInts = map (fst . fromJust . C.readInt) . C.words <$> C.getLine
 
 main :: IO ()
 main = do
-  [_,k] <- map read . words <$> getLine
-  ls    <- map read . words <$> getLine
-  print $ maxSnuke k ls
+  [_,k] <- readInts 
+  ls    <- readInts 
+--  print $ maxSnuke k ls
+  print $ solve ls k 
 
 maxSnuke
   :: Int -> [Int] -> Int
 maxSnuke k = sum . take k . sortBy (flip compare)
+
+-- https://atcoder.jp/contests/abc067/submissions/4912288
+solve :: [Int] -> Int -> Int
+solve = (sum .) . flip take . sortBy (flip compare) 
